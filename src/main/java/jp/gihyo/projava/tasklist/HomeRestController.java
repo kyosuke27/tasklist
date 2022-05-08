cca2c73d5f67512ac8b0667fd6ace74c2c1aaee5
kyousuke <kn1359028527@gmail.com>
2022-05-08 16:44:58 +0900

    タスクの修正機能追加

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,5 +1,6 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,18 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import jp.gihyo.projava.tasklist.TaskListDao;
+
 @Controller
 public class HomeRestController {
+
+    private final TaskListDao dao;
+
+    @Autowired
+    HomeRestController(TaskListDao dao) {
+        this.dao = dao;
+    }
+
     record TaskItem(String id, String task, String deadline, boolean done) {
     }
 
@@ -31,14 +42,32 @@ public class HomeRestController {
     String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
         String id = UUID.randomUUID().toString().substring(0, 8);
         TaskItem item = new TaskItem(id, task, deadline, false);
-        taskItems.add(item);
+        dao.add(item);
 
         return "redirect:/list";
     }
 
     @GetMapping("/list")
     String listItems(Model model) {
+        List<TaskItem> taskItems = dao.findAll();
         model.addAttribute("taskList", taskItems);
         return "home";
+    }
+
+    @GetMapping("/delete")
+    String deleteItem(@RequestParam("id") String id) {
+        dao.delete(id);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/update")
+    String updateItem(@RequestParam("id") String id,
+                      @RequestParam("task") String task,
+                      @RequestParam("deadline") String deadline,
+                      @RequestParam("done") boolean done) {
+        TaskItem taskItem = new TaskItem(id, task, deadline, done);
+        System.out.println("デバッグ");
+        dao.update(taskItem);
+        return "redirect:/list";
     }
 }
